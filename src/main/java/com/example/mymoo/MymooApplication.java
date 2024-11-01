a1884cb383cc6d98978051e3c195a9e9ac30e563
hyeneung <imasaru22@gmail.com>
2024-11-01 16:24:23 +0900

    first commit

---
+++ src/main/java/com/example/mymoo/MymooApplication.java
@@ -0,0 +1,13 @@
+package com.example.mymoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymooApplication.class, args);
+	}
+
+}
