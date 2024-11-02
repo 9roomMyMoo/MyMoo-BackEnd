f0f853216a9ddcbc45157b93c3922f8e9e78ad8e
hyeneung <imasaru22@gmail.com>
2024-11-03 00:45:31 +0900

    #1 - feature: Kakao social login using WebClient and add required dependencies
    - Developed Kakao social login functionality utilizing WebClient for API communication.
    - Added necessary dependencies to support Kakao login integration.

---
+++ src/main/java/com/example/mymoo/global/oauth/service/impl/OauthServiceImpl.java
@@ -0,0 +1,145 @@
+package com.example.mymoo.global.oauth.service.impl;
+
+import com.example.mymoo.domain.account.entity.Account;
+import com.example.mymoo.domain.account.repository.AccountRepository;
+import com.example.mymoo.global.auth.repository.AuthRepository;
+import com.example.mymoo.global.oauth.dto.kakao.KakaoTokenResponse;
+import com.example.mymoo.global.oauth.dto.kakao.KakaoUserInfo;
+import com.example.mymoo.global.oauth.dto.response.KakaoLoginResponseDto;
+import com.example.mymoo.global.oauth.service.OAuthService;
+import com.example.mymoo.global.security.jwt.JwtTokenProvider;
+import java.util.Optional;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class OauthServiceImpl implements OAuthService {
+
+    @Value("${kakao.client-id}")
+    private String kakaoClientId;
+
+    @Value("${kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+
+    @Value("${kakao.authorization-uri}")
+    private String kakaoAuthorizationUri;
+
+    @Value("${kakao.token-uri}")
+    private String kakaoTokenUri;
+
+    @Value("${kakao.user-info-uri}")
+    private String kakaoUserInfoUri;
+
+    private final WebClient webClient;
+    private final AccountRepository accountRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final AuthRepository authRepository;
+
+    @Override
+    public String getKakaoAuthorizationUrl() {
+        return UriComponentsBuilder.fromUriString(kakaoAuthorizationUri)
+            .queryParam("response_type", "code")
+            .queryParam("client_id", kakaoClientId)
+            .queryParam("redirect_uri", kakaoRedirectUri) // TODO - 배포 시 변경
+            .queryParam("scope", "account_email")
+            .toUriString();
+    }
+
+    @Override
+    public KakaoLoginResponseDto processKakaoCallback(String code) {
+        // 카카오 액세스 토큰 요청
+        String kakaoAccessToken = getAccessToken(code);
+        log.info("Received kakao access token: {}", kakaoAccessToken);
+
+        // 사용자 정보 요청
+        KakaoUserInfo userInfo = getUserInfo(kakaoAccessToken);
+        log.info("Received kakao user info: {}", userInfo);
+
+        // 신규 유저인지 확인
+        String kakaoUserEmail = userInfo.id() + "_" + userInfo.kakaoAccount().email();
+        Optional<Account> accountOpt = accountRepository.findByEmail(kakaoUserEmail);
+
+        if (accountOpt.isEmpty()) {
+            // 신규 유저라면 계정 생성
+            Account newAccount = createNewAccount(userInfo);
+            accountRepository.save(newAccount);
+
+            return handleLogin(newAccount, true);
+        } else {
+            Account existingAccount = accountOpt.get();
+            return handleLogin(existingAccount, false);
+        }
+    }
+
+    // https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#request-token
+    private String getAccessToken(String code) {
+        log.info("processing code: {}", code);
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add("grant_type", "authorization_code");
+        formData.add("client_id", kakaoClientId);
+        formData.add("redirect_uri", kakaoRedirectUri);
+        formData.add("code", code);
+        return webClient.post()
+            .uri(kakaoTokenUri)
+            .header("Content-Type", "application/x-www-form-urlencoded;charset=utf-8")
+            .body(BodyInserters.fromFormData(formData))
+            .retrieve()
+            .bodyToMono(KakaoTokenResponse.class)
+            .map(KakaoTokenResponse::accessToken)
+            .block(); // 동기 요청
+    }
+
+    // https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
+    private KakaoUserInfo getUserInfo(String accessToken) {
+        return webClient.post()
+            .uri(kakaoUserInfoUri)
+            .header("Authorization", "Bearer " + accessToken)
+            .header("Content-type", "application/x-www-form-urlencoded;charset=utf-8")
+            .body(BodyInserters.fromFormData("property_keys", "[\"kakao_account.email\"]"))
+            .retrieve()
+            .bodyToMono(KakaoUserInfo.class)
+            .block(); // 동기 요청
+    }
+
+    private Account createNewAccount(KakaoUserInfo userInfo) {
+        String kakaoUserEmail = userInfo.id() + "_" + userInfo.kakaoAccount().email();
+        String password = passwordEncoder.encode(UUID.randomUUID().toString());
+        return Account.builder()
+            .email(kakaoUserEmail)
+            .password(password)
+            .build();
+    }
+
+    private KakaoLoginResponseDto handleLogin(Account account, boolean isNewUser) {
+        // 토큰 생성
+        String accessToken = jwtTokenProvider.createAccessTokenWithAccountEntity(account);
+        String refreshToken = jwtTokenProvider.createRefreshTokenWithAccountEntity(account);
+
+        // refresh 토큰 저장
+        authRepository.saveRefreshToken(
+            account.getId(),
+            refreshToken,
+            JwtTokenProvider.REFRESH_TOKEN_VALIDITY
+        );
+
+        // Jwt 및 로그인 정보 전달
+        return KakaoLoginResponseDto.builder()
+            .accountId(account.getId())
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .isNewUser(isNewUser)
+            .build();
+    }
+}
