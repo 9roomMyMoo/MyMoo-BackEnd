465f324a0039d2108b4f5235dd0ebd26fb14ad08
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-11-19 20:17:45 +0900

    devOps: ci/cd setting
    - ci/cd setting

---
+++ init/init.sql
@@ -0,0 +1,6 @@
+create database mymoo;
+use mymoo;
+-- create user 'hyeneung'@'localhost' identified by 'thisispassword';
+-- grant all on `extra`.* to 'hyeneung'@'localhost' with grant option;
+--
+-- flush privileges;
